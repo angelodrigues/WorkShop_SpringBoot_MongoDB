7e6b4ad15b656db4d68e7c102e80ad0efd7d2958
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-18 14:19:13 -0300

    First Commit

---
+++ src/test/java/com/angelodrigues/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelodrigues.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2ab3d0dfadc743b6beb08faca60d1fb44ca5b53e
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-18 19:02:31 -0300

    Endpoint to return a user's posts

--- src/main/java/com/angelodrigues/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/angelodrigues/workshopmongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.angelodrigues.workshopmongodb.domain.Post;
 import com.angelodrigues.workshopmongodb.domain.User;
 import com.angelodrigues.workshopmongodb.dto.UserDTO;
 import com.angelodrigues.workshopmongodb.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);    
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPostById(@PathVariable String id){
+        User obj = service.findById(id);         
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
